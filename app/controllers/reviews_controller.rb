fbbf6818fb393eeabd1b6f7825e5b69a3283cb60
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-16 16:23:11 +0900

    problem1

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,5 @@
 class ReviewsController < RankingController
-  before_action :authenticate_user!, only: :new
-
+  
   def new
     @product = Product.find params[:product_id]
     @review = Review.new
